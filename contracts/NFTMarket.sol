236ae542494837dadb6fa09abfc6c3821ae4c458
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2023-01-12 19:15:04 +0530

    Added openzepplin contract

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NFTMarket{
-    
+contract NFTMarket is ERC721URIStorage{
+    constructor() ERC721("BhartiyaNFT", "CNFT"){
+
+    }
 }
