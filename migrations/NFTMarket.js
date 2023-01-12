78a70ad7bdcd5d353600be342af37966f435f77f
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2023-01-12 19:55:09 +0530

    trying to get the contract deployed properly

--- migrations/NFTMarket.js
+++ migrations/NFTMarket.js
@@ -1,5 +1,18 @@
 const NftMarket = artifacts.require("NFTMarket");
 
 module.exports = function (deployer) {
-  deployer.deploy(NftMarket);
+    const contractName = "NFTMarket"
+    deployer.deploy(NftMarket)
+    
+    
+    // .then(() => {
+    //     const artifact = artifacts.require(`${contractName}.sol`);
+    //     console.log(artifact)
+    //     const metaDataFile = `${__dirname}/public/contracts/${contractName}.json`;
+    //     const metaData = require(metaDataFile);
+    //     metaData.networks[deployer.network_id] = {};
+    //     metaData.networks[deployer.network_id].address = artifact.address;
+    //     fs.writeFileSync(metaDataFile, JSON.stringify(metaData, null, 4));
+    // })
+
 };
