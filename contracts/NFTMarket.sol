91d02f7690dc3250f6f49e825da856da7454b0f1
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2023-02-20 09:26:18 +0530

    fixed the migration issues

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity >=0.4.22 <0.9.0;
+ 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
-contract NFTMarket is ERC721URIStorage{
-    constructor() ERC721("BhartiyaNFT", "CNFT"){
-
-    }
+ 
+contract NftMarket is ERC721URIStorage {
+  constructor() ERC721("BhartiyaNFT", "CNFT") {}
 }
